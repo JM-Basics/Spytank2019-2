7e00d1763725a39bf94bd9647509698cf64f9078
Jean-Michel <jeanmi2010@hotmail.be>
2019-12-16 15:09:00 +0100

    finalisation remote clavier

--- basic_remote/remoteClavier.py
+++ basic_remote/remoteClavier.py
@@ -1,10 +1,32 @@
 import spytank
 
+z ="z : avancer" 
+s ="s : reculer"
+q ="q : tourner à gauche"
+d ="d : tourner à droite"
+vp ="a : plus vite" 
+vm ="e : moins vite"
+x ="x : stop"
+vitesse = 150
+
+print(z,s,q,d,vp,vm,x)
+
 while True:
     lettre = input(">>> ")
     if lettre == "z" : 
-        spytank.avance(150)
+        spytank.avance(vitesse)
     elif lettre == "s" :
-        spytank.recule(150)
+        spytank.recule(vitesse)
+    elif lettre == "q" :
+        spytank.gauche(vitesse)
+    elif lettre == "d" : 
+        spytank.droite(vitesse)
+    elif lettre == "a" :
+        if vitesse < 250 :
+            vitesse = vitesse + 20
+    elif lettre == "e" :
+        if vitesse < 70 : 
+        vitesse = vitesse -20
     elif lettre == "x" :   
         spytank.stop()
+    
